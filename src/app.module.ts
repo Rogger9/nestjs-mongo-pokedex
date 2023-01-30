e9e2cde87989d3010f41977af103138e8b5976dd
Rogelio Gutierrez <gutierrez.rog12@gmail.com>
2023-01-30 07:36:18 -0300

    feat: implement static serve

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { ServeStaticModule } from '@nestjs/serve-static'
+import { join } from 'path'
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ServeStaticModule.forRoot({ rootPath: join(__dirname, '..', 'public') })],
 })
 export class AppModule {}
