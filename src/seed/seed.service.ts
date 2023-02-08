f1800703cf7344685eba4c963dd897e96ad4bbeb
Rogelio Gutierrez <gutierrez.rog12@gmail.com>
2023-02-08 11:41:33 -0300

    feat: create http adapter

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common'
 import { InjectModel } from '@nestjs/mongoose'
-import axios, { AxiosInstance } from 'axios'
 import { Model } from 'mongoose'
+import { AxiosAdapter } from 'src/common/adapters/axios.adapter'
 import { Pokemon } from 'src/pokemon/entities/pokemon.entity'
 import { PokeResponse } from './interfaces/poke-res.interface'
 
@@ -9,12 +9,13 @@ const LIMIT = 10
 
 @Injectable()
 export class SeedService {
-  constructor(@InjectModel(Pokemon.name) private readonly pokemonModel: Model<Pokemon>) {}
-
-  private readonly axios: AxiosInstance = axios
+  constructor(
+    @InjectModel(Pokemon.name) private readonly pokemonModel: Model<Pokemon>,
+    private readonly http: AxiosAdapter,
+  ) {}
 
   private async getData() {
-    const { data } = await this.axios.get<PokeResponse>(`https://pokeapi.co/api/v2/pokemon?limit=${LIMIT}`)
+    const data = await this.http.get<PokeResponse>(`https://pokeapi.co/api/v2/pokemon?limit=${LIMIT}`)
 
     const pokemons = data.results.map(({ name, url }) => {
       const no = +url.split('/').at(-2)
